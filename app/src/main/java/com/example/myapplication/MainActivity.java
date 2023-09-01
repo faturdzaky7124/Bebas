254482cf8d934f4caebe88c69ede8b4c139a6ba8
faturdzaky7124 <faturrahmandzaky2@gmail.com>
2023-09-01 09:40:36 +0700

    First Android Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtName = findViewById(R.id.txt_name);
+        txtName.setText("Mobile Apps TIF'22 BWS");
+    }
+}
